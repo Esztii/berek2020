fa4645731554dd86c061cb1c90fd229c5d012ef7
orosz.eszter <orosz.eszter@28-DIAK7>
2024-11-18 10:06:42 +0100

    Add project files.

---
+++ berek2020/Berek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+    internal class Berek
+    {
+        public string Nev { get; set; }
+        public bool Neme { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nev}: {(Neme ? "férfi" : "nő")}, {Reszleg}, {Belepes}, {Ber}";
+        }
+
+        public Berek(string sor) 
+        {
+            string[] db = sor.Split(";");
+            Nev = db[0];
+            Neme = db[1] == "férfi";
+            Reszleg = db[2];
+            Belepes = int.Parse(db[3]);
+            Ber = int.Parse(db[4]);
+        }
+
+    }
+}
---
+++ berek2020/Program.cs
@@ -0,0 +1,31 @@
+﻿using berek2020;
+using System.Text;
+
+List<Berek> bereks = [];
+using StreamReader sr = new("..\\..\\..\\src\\berek2020.txt", Encoding.UTF8);
+sr.ReadLine();
+while(!sr.EndOfStream) bereks.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. feldat: Dolgozók száma: {bereks.Count} fő");
+
+var f4 = bereks.Average(b => b.Ber);
+Console.WriteLine($"4. feldat: 2020-as átlagbérek: {f4:0.0}FT");
+
+Console.Write("5. feladat: Kérem a részleg nevét:");
+string reszleg = Console.ReadLine();
+
+Console.WriteLine("6. feladat: A legtöbbet kereső dolgozó a részlegen:");
+var reszlegendolgozok = bereks.FindAll(e => e.Reszleg == reszleg);
+
+var max = reszlegendolgozok.MaxBy(e => e.Ber);
+Console.WriteLine($"\tNév: {max.Nev} \n\tNem: {max.Neme} \n\tBelépés: {max.Belepes} \n\tBér: {max.Ber} FT");
+
+
+
+Console.WriteLine("7. feldat: Statisztika");
+var statisztika = bereks.GroupBy(b => b.Reszleg).OrderBy(b => b.Key);
+foreach(var grp in statisztika) Console.WriteLine(
+    $"\t{grp.Key} - "+ 
+    $"{grp.Count()} fő" );
+  
+
